287c3ff95a861a25a4448b5c91775fff95f7da92
jimbitol <maxcabrera815@gmail.com>
2017-01-28 13:23:56 +1100

    File input handler - I can read now

--- rea-challenge/src/main/java/com/cabreramax/challenge/handlers/FileInputHandler.java
+++ rea-challenge/src/main/java/com/cabreramax/challenge/handlers/FileInputHandler.java
@@ -1,13 +1,48 @@
 package com.cabreramax.challenge.handlers;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileInputHandler {
 
-	public List<String> handleInput(String cOMMANDS_INPUT_FILE_PATH) {
+	public List<String> handleInput( String inputsPath ) throws FileNotFoundException {
+			
+		List<String> inputsList = new ArrayList<String>();
 		
-		return new ArrayList<String>();
+		BufferedReader br = null;
+		FileReader fr = null;
+		
+		try {
+			
+			br = new BufferedReader( new FileReader( inputsPath ) );
+			
+            String line;
+            
+            while ( (line = br.readLine()) != null ) {
+            	inputsList.add(line);
+            }
+            
+        } catch (IOException e) {
+            
+        	throw new FileNotFoundException("Invalid Path");
+            
+        } finally {
+        	
+        	try { 
+        		
+        		if ( br != null ) br.close();
+        		if ( fr != null ) fr.close();
+        		
+        	} catch ( Exception e ) {
+        		e.printStackTrace();
+        	}
+        }
+		
+		return inputsList;
 	}
 
 }
