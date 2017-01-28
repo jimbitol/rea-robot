989f2ae2ced12c1fef2a467fbd592f4ca96ef031
jimbitol <maxcabrera815@gmail.com>
2017-01-28 12:52:59 +1100

    making tests for input reader

---
+++ rea-challenge/src/main/java/com/cabreramax/challenge/handlers/FileInputHandler.java
@@ -0,0 +1,13 @@
+package com.cabreramax.challenge.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileInputHandler {
+
+	public List<String> handleInput(String cOMMANDS_INPUT_FILE_PATH) {
+		
+		return new ArrayList<String>();
+	}
+
+}
---
+++ rea-challenge/src/test/java/com/cabreramax/challenge/handlers/FileInputHandlerTest.java
@@ -0,0 +1,87 @@
+package com.cabreramax.challenge.handlers;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FileInputHandlerTest {
+
+	private final String COMMANDS_INPUT_FILE_PATH = getClass().getResource("/commands_inputs.txt").getPath();
+
+    @Test
+    public void testWhenValidFileProvidedThenReadWithoutException() {
+        
+        try {
+        	
+        	(new FileInputHandler()).handleInput(COMMANDS_INPUT_FILE_PATH);
+
+        	assertTrue( true );
+        
+        } catch ( Exception e ) {
+        	fail("Should read File");
+        }
+    }
+
+    @Test
+    public void testWhenValidFileProvidedThenReturnArrayWithStringInputs() {
+        
+        try {
+        	
+        	List<String> inputsListExpected = buildInputListExpected();
+        	
+        	assertArrayEquals( inputsListExpected.toArray(), (new FileInputHandler()).handleInput(COMMANDS_INPUT_FILE_PATH).toArray() );
+        
+        } catch ( Exception e ) {
+        	fail("Should read File");
+        }
+    }
+
+	@Test
+    public void testWhenInvalidFileProvidedThenThrowException() {
+        
+        try {
+        	
+        	(new FileInputHandler()).handleInput("//invalid/file/path");
+
+        	fail("Should not read File");
+        
+        } catch ( Exception e ) {
+        	
+        	assertEquals(e.getMessage(), "Invalid Path");	
+        }
+    }
+    
+    private List<String> buildInputListExpected() {
+		
+		return new ArrayList<String>(
+    			
+    			Arrays.asList(
+    					new String("MOVE"),
+    					new String("LEFT"),
+    					new String("JOE"),
+    					new String("RIGHT"),
+    					new String("REPORT"),
+    					new String("PLACE 0,0,NORTH"),
+    					new String("REPORT"),
+    					new String("MOVE"),
+    					new String("LOL"),
+    					new String("LEFT"),
+    					new String("MOVE"),
+    					new String("LEFT"),
+    					new String("MOVE"),
+    					new String("REPORT"),
+    					new String("PLACE 10,9,NORTH"),
+    					new String("REPORT"),
+    					new String("PLACE 3,4,JOHN"),
+    					new String("REPORT"),
+    					new String("PLACE 4,3,EAST"),
+    					new String("MOVE"),
+    					new String("REPORT")
+    			)
+    	);
+	}
+}
